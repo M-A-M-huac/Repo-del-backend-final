ca1d7af1fc2e8ac980d992584af0dfd797c20909
M-A-M-huac <matias.angelico@anahuacmayab.edu.mx>
2026-04-24 17:45:51 -0600

    Update server.js
    no sirvió de nada

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send('API is running')
-})
+
 
 app.use('/api/users', userRoutes)
 app.use('/api/reviews', reviewRoutes)
